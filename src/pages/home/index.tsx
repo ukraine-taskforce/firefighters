0206e20d40c868be0d5f8efbbb1b609ad0afa61a
AntonRaichuk <raveman.net@gmail.com>
2022-04-05 10:13:51 +0000

    Add left textual table

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,28 @@
-import { Map } from '../../others/components/map/Map';
+import { Map } from "../../others/components/map/Map";
+import { CollapsibleTable } from "../../others/components/CollapsibleList";
+import { Sidebar } from "../../others/components/Sidebar";
+import { Main } from "../../others/components/Main";
 import { MapProvider } from "react-map-gl";
+import { fakeData } from "../../others/fixtures/fakeDonations";
 import Box from "@mui/material/Box";
 
 export function Home() {
+  const sortedData = fakeData.sort((a, b) => b.total - a.total);
+  const tableData = sortedData.map((data) => {
+    return {
+      name: data.name,
+      value: data.total,
+      description: data.description,
+      coordinates: { latitude: data.lat, longitude: data.long },
+    };
+  });
+  const table = (
+    <CollapsibleTable
+      rows={tableData}
+      renderRowData={(row) => { return row; } }
+    />
+  );
+
   return (<>
             <h1>Feuer löschen, Menschen retten - mit Deiner Hilfe</h1>
 	    <h3>Es brennt in der Ukraine. Immer wieder richten die russischen Angriffe Zerstörung und Leid an
@@ -11,8 +31,10 @@ export function Home() {
 	    Wie viele Spenden eingenommen wurden und wie viel Ausgaben 
 	    </h3>
             <MapProvider>
-              <Box sx={{height: "calc(100vh - 200px)", maxHeight: "100vh", overflowY: "hidden" }} >
-                <Map />
+              <Box sx={{ height: "calc(100vh - 200px)", maxHeight: "100vh", overflowY: "hidden" }}>
+                <Main aside={<Sidebar className="requests-sidebar">{table}</Sidebar>}>
+                  <Map />
+                </Main>
               </Box>
             </MapProvider>
           </>);
