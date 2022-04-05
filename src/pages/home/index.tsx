76504f5432979f6d66acbadd7299ab68be470757
AntonRaichuk <raveman.net@gmail.com>
2022-04-05 09:29:08 +0000

    Add fake data to the map

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,9 +4,14 @@ import Box from "@mui/material/Box";
 
 export function Home() {
   return (<>
-            <h1>Let's help firefighters!!!</h1>
+            <h1>Feuer löschen, Menschen retten - mit Deiner Hilfe</h1>
+	    <h3>Es brennt in der Ukraine. Immer wieder richten die russischen Angriffe Zerstörung und Leid an
+	    Die Menschen in der Ukraine benötigen unsere Unterstützung. Mit dem gesammelten Geld wollen wir die ukrainische Feuerwehr unterstützen, die unter Einsatz ihres Lebens Brände löscht, Menschen aus Trümmern rettet und die</h3>
+	    <h3>Die Spenden werden genutzt, um die Feuerwehr in der Ukraine zu unterstützen und 
+	    Wie viele Spenden eingenommen wurden und wie viel Ausgaben 
+	    </h3>
             <MapProvider>
-              <Box sx={{height: "100vh", maxHeight: "100vh", overflowY: "hidden" }} >
+              <Box sx={{height: "calc(100vh - 200px)", maxHeight: "100vh", overflowY: "hidden" }} >
                 <Map />
               </Box>
             </MapProvider>
